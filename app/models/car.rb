941b5eea9009dad5ac3c870b1a7c1c87696b9578
Andrew Carmer <carmerandrew@gmail.com>
2016-06-10 10:38:30 -0600

    implemented with scopes

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,38 +1,11 @@
 class Car < ActiveRecord::Base
-
-  def self.alphabetical_by_make
-    self.order(:make)
-  end
-
-  def self.make(make)
-    where(make: make)
-  end
-
-  def self.model(model)
-    where(model: model)
-  end
-
-  def self.newer_than(year)
-    where("year > ?", year)
-  end
-
-  def self.older_than(year)
-    where("year < ?", year)
-  end
-
-  def self.rear_wheel_drive
-    where(drive: "Rear-Wheel Drive")
-  end
-
-  def self.front_wheel_drive
-    where(drive: "Front-Wheel Drive")
-  end
-
-  def self.city_mpg_less_than(mpg)
-    where("city_mpg < ?", mpg)
-  end
-
-  def self.city_mpg_more_than(mpg)
-    where("city_mpg > ?", mpg)
-  end
+  scope :alphabetical_by_make, -> { order(:make) }
+  scope :model, -> (model)  {where(model: model )}
+  scope :make, -> (make) { where(make: make )}
+  scope :newer_than, -> (year) { where("year > ?", year)}
+  scope :older_than, -> (year) { where("year < ?", year)}
+  scope :rear_wheel_drive, -> { where(drive: "Rear-Wheel Drive")}
+  scope :front_wheel_drive, -> { where(drive: "Front-Wheel Drive")}
+  scope :city_mpg_less_than, -> (mpg) { where("city_mpg < ?", mpg)}
+  scope :city_mpg_more_than, -> (mpg) { where("city_mpg > ?", mpg)}
 end
